35c4151e67d30b373e36c96751a50ac689f20486
carolinaferpereira <carolinaferpereira@gmail.com>
2023-09-14 20:05:27 -0300

    Assignment update

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your grade percentage: ");
+        string valueFromUser = Console.ReadLine();
+        int value = int.Parse(valueFromUser);
+
+        string letter = "";
+
+        if (value >= 90)
+        {
+            letter = "A";
+        }
+        else if (value >= 80)
+        {
+            letter = "B";
+        }
+        else if (value >= 70)
+        {
+            letter = "C";
+        }
+        else if (value >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is {letter}");
+
+        if (value >= 70)
+        {   
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("You failed. Better luck next time.");
+        }
     }
 }
